8bb612a6d49a22424fe05db2f82474d390a195f1
claudio cavallaro <claudio.cavallaro91@gmail.com>
2021-02-21 00:19:47 +0100

    Fix docker compose

--- demodb/start.sh
+++ demodb/start.sh
@@ -1,3 +1,6 @@
+echo "--------------Maven clean install -> creating jar -----------------"
+mvn clean install -DskipTests;
+
 docker-compose -p project -f docker-compose.yml up -d db;
 sleep 10;
 docker-compose -p project -f docker-compose.yml up -d app;
