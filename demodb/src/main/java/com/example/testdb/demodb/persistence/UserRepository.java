e755335b28329367380876dda151b5e5473f973b
claudio cavallaro <claudio.cavallaro91@gmail.com>
2021-02-27 23:32:14 +0100

    fix

--- demodb/src/main/java/com/example/testdb/demodb/persistence/UserRepository.java
+++ demodb/src/main/java/com/example/testdb/demodb/persistence/UserRepository.java
@@ -1,10 +1,11 @@
 package com.example.testdb.demodb.persistence;
 
 import com.example.testdb.demodb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "select nome \n" +
             "from userdb\n" +
